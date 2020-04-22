2fe523f835c73630db9e717c981c4146f2ac2cc7
Yongshan Tan <yongshantan1024@gmail.com>
2020-04-22 16:33:18 -0500

    fixed postWeatherJournal request temperature undefined problem.

--- website/app.js
+++ website/app.js
@@ -2,10 +2,6 @@
 let baseURL = "http://api.openweathermap.org/data/2.5/weather?";
 let OpenWeatherApiKey = "edc8da4ad2db633b4f4474a6434dfc6a";
 let apiKey = `&appid=${OpenWeatherApiKey}`;
-// data that get from OpenWeather API
-let weatherJournalData;
-// temperature that get from data object
-let temp;
 
 // Click Event handler for generate button
 document.getElementById('generate').addEventListener('click', performAction);
@@ -21,19 +17,15 @@ async function performAction(e) {
     // get user response of "How are you feeling today?"
     const userResponse = document.getElementById("feelings").value;
 
-    // console.log('newZipCode', newZipCode);
-    // console.log('newDate', newDate);
-    // console.log('url', baseURL + newZipCode + apiKey);
-
     // chain the promises
     getWeatherJournal(baseURL, newZipCode, apiKey)
-        .then(
-            await postWeatherJournal('/addData', {
-                temperature: temp,
+        .then(function (weatherJournalData) {
+            postWeatherJournal('/addData', {
+                temperature: weatherJournalData.main['temp'],
                 date: newDate,
                 userResponse: userResponse
-            })
-        )
+            });
+        })
         .then(
             updateUI
         )
@@ -43,11 +35,8 @@ async function performAction(e) {
 const getWeatherJournal = async(baseURL, zipCode, apiKey) => {
     const request = await fetch(baseURL+zipCode+apiKey);
     try{
-        weatherJournalData = await request.json();
-        temp = weatherJournalData.main['temp'];
-        // console.log('url', baseURL+zipCode+apiKey)
-        // console.log('data',weatherJournalData);
-        // console.log('temp', temp);
+        const weatherJournalData = await request.json();
+        return weatherJournalData;
     }catch (e) {
         console.log('error', e);
     }
